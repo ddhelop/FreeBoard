d9f82370ca5f1426e4bd9d7fff72d2ddd22c667c
김동혁 <clfgnm9@naver.com>
2023-11-01 16:46:07 +0900

    add layout banner UI

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -12,7 +12,9 @@ export default function App({ Component, pageProps }: AppProps) {
       <>
         <Global styles={globalStyles}/>
         <Layout>
+          <div>
           <Component {...pageProps} />
+          </div>
         </Layout>
       </>
     </ApolloSetting>
--- src/components/commons/layout/banner/LayoutBanner.presenter.tsx
+++ src/components/commons/layout/banner/LayoutBanner.presenter.tsx
@@ -1,7 +1,16 @@
+import * as B from "./LayoutBanner.styles"
+
 export default function LayoutBannerUI(){
   
   return(
     <>
-
+      <B.Wrapper>
+        <B.BannerImg src="/banner/image.png"/>
+        <B.MenuWrapper>
+          <B.Button>자유게시판</B.Button>
+          <B.CenterButton>중고마켓</B.CenterButton>
+          <B.Button>마이페이지</B.Button>
+        </B.MenuWrapper>
+      </B.Wrapper>
     </>
   )}
